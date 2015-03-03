24de03f45567894f73bde435f8e694bcf6d412e3
Jeff Hostetler <hostetjm@gmail.com>
2015-03-02 17:31:54 -0700

    basic computer plays against you

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var winners = [
                 ["one", "five", "nine"], ["three", "five", "seven"]
               ]
 
+var available_cells = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 var x_array =[];
 var o_array = [];
+var winnerCheck = false;
+
 
 function diff(arr1, arr2) {
     var ret = [];
@@ -19,46 +22,60 @@ function diff(arr1, arr2) {
     return ret;
 };
 
+function removeA(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
+};
+
+function checkWinner(arr1, arr2){
+  arr1.forEach(function(entry){
+    if (diff(entry, arr2).length == 3){
+      winnerCheck = true;
+      console.log("Winner");
+      $(".waiting").removeClass("waiting");
+      entry.forEach(function(id){
+        $('#'+id).addClass("winner");
+      })
+      $('.cell').each(function(){
+        if ($(this).hasClass("winner") != true) {
+          $(this).addClass("loser")
+        }
+      })
+    }
+  })
+}
+
+function runComputer(){
+  var computer_choice = available_cells[Math.floor(Math.random()*available_cells.length)];
+  console.log(computer_choice)
+  $('#' + computer_choice).append("<p>X</p>")
+  click_count +=1;
+  $('#' + computer_choice).removeClass("waiting");
+  x_array.push(computer_choice);
+  removeA(available_cells, computer_choice)
+  checkWinner(winners, x_array)
+}
+
 $( ".waiting" ).click(function() {
   if ($(this).hasClass("waiting")) {
     if (click_count % 2 ==0) {
       $(this).append( "<p>O</p>" );
       click_count +=1;
-      $(this).removeClass("waiting");
+      $(this).removeClass("waiting").delay( 800 );
       o_array.push(this.id);
-      winners.forEach(function(entry){
-        if (diff(entry, o_array).length == 3){;
-          console.log("Winner");
-          $(".waiting").removeClass("waiting");
-          entry.forEach(function(id){
-            $('#'+id).addClass("winner");
-          })
-          $('.cell').each(function(){
-            if ($(this).hasClass("winner") != true) {
-              $(this).addClass("loser")
-            }
-          })
-        }
-      })
-    } else {
-      $(this).append( "<p>X</p>" );
-      click_count +=1;
-      $(this).removeClass("waiting");
-      x_array.push(this.id);
-      winners.forEach(function(entry){
-        if (diff(entry, x_array).length == 3){
-          console.log("Winner");
-          $(".waiting").removeClass("waiting");
-          entry.forEach(function(id){
-            $('#'+id).addClass("winner");
-          })
-          $('.cell').each(function(){
-            if ($(this).hasClass("winner") != true) {
-              $(this).addClass("loser")
-            }
-          })
-        };
-      })
+      removeA(available_cells, this.id)
+      checkWinner(winners, o_array)
+      if (winnerCheck==false) {
+        window.setTimeout(runComputer,500)
+      }
+
+
     }
   }
 });
